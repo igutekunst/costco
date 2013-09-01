abb4b22d8c4814fa14ac01043118490736472c93
Isaac Gutekunst <igutekunst@gmail.com>
2013-09-01 16:44:22 -0400

    Re-org and added basic tabbing

--- costco.js
+++ costco.js
@@ -38,10 +38,10 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     if (Products.find().count() === 0) {
-      var prods = [{name: "Template",   itemNumber :-1, price :-1,    size : 0,  unit : "can"},
+      var prods = [{name: "Brocoli",   itemNumber :  4, price : 1,    size : 1,  unit : "can"},
                    {name : "Diet Coke", itemNumber : 0, price : 9.59, size : 32, unit : "can"},
                    {name : "Root Beer", itemNumber : 1, price : 9.59, size : 32, unit : "can"},
-                   {name : "Cheese"   , itemNumber : 2, price : 0,    size : 1,  unit : "wedge"}
+                   {name : "Cheese"   , itemNumber : 2, price : 10,    size : 1,  unit : "block"}
                   ];
       for (var i = 0; i < prods.length; i++)
         Products.insert(prods[i]);
