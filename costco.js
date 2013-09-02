cc6c3d74db909b90c58d2c281983cca81a904b6b
Isaac Gutekunst <igutekunst@gmail.com>
2013-09-02 19:20:07 -0400

    Products code in seperate folder

--- costco.js
+++ costco.js
@@ -19,34 +19,6 @@ if (Meteor.isClient) {
     }
 
 
-  Template.products.products = function () {
-    return Products.find({}, {sort: {price: 1, name: 1}});
-  };
-
-  Template.products.selected_product = function () {
-    var product = Products.findOne(Session.get("selected_product"));
-    return product && product.name;
-  };
-
-  Template.product.selected = function () {
-    return Session.equals("selected_product", this._id) ? "selected" : '';
-  };
-
-  Template.products.events({
-    'click input.inc': function () {
-      Products.update(Session.get("selected_product"), {$inc: {price: 5}});
-    }
-  });
-
-  Template.product.events({
-    'click': function () {
-      Session.set("selected_product", this._id);
-    }
-  });
-
-  Template.product.unitPrice = function() {
-   return this.price / this.size
-  };
     items = {};
      nav_items.map(function (item) {
         items[item.link] = item.link.slice(1);
